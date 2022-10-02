73c874d947f13d4251db498e18e599c39e554363
Nicholas Blanco <nablanco92@gmail.com>
2022-10-02 17:14:37 -0500

    Add technology tablet styling

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -18,6 +18,11 @@ const Header = styled.div`
   align-items: center;
   margin-top: 24px;
   margin-bottom: 32px;
+
+  @media screen and (min-width: 768px) {
+    align-self: flex-start;
+    margin: 40px 0px 60px 38.5px;
+  }
 `;
 const HeaderNumber = styled.div`
   font-style: normal;
@@ -27,6 +32,11 @@ const HeaderNumber = styled.div`
   letter-spacing: 2.7px;
   opacity: 0.25;
   margin-right: 18px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+    line-height: 24px;
+  }
 `;
 const HeaderText = styled.div`
   font-style: normal;
@@ -35,6 +45,11 @@ const HeaderText = styled.div`
   line-height: 19px;
   letter-spacing: 2.7px;
   text-transform: uppercase;
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+    line-height: 24px;
+  }
 `;
 const TechnologyImageContainer = styled.div`
   display: flex;
@@ -44,10 +59,21 @@ const TechnologyImageContainer = styled.div`
   height: 170px;
   margin-bottom: 26px;
   object-fit: contain;
+
+  @media screen and (min-width: 768px) {
+    width: 100%;
+    height: 310px;
+    margin-bottom: 56px;
+  }
 `;
 const TechnologyImage = styled.img`
   height: 170px;
   object-fit: contain;
+
+  @media screen and (min-width: 768px) {
+    width: 100%;
+    height: 310px;
+  }
 `;
 const TechnologyInfoContainer = styled.div`
   display: flex;
@@ -55,6 +81,10 @@ const TechnologyInfoContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 327px;
+
+  @media screen and (min-width: 768px) {
+    width: 458px;
+  }
 `;
 const TechnologyButtonContainer = styled.div`
   display: flex;
@@ -62,8 +92,16 @@ const TechnologyButtonContainer = styled.div`
   justify-content: space-between;
   width: 152px;
   margin-bottom: 26px;
+
+  @media screen and (min-width: 768px) {
+    width: 210px;
+    margin-bottom: 44px;
+  }
 `;
 const TechnologyButton = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 40px;
   height: 40px;
   border-radius: 50%;
@@ -77,7 +115,7 @@ const TechnologyButton = styled.a`
   font-weight: 400;
   font-size: 16px;
   line-height: 18px;
-  text-align: center;
+  /* text-align: center; */
   letter-spacing: 1px;
 
   &:hover {
@@ -87,6 +125,13 @@ const TechnologyButton = styled.a`
 
   color: var(${(props) => (props.isActive ? "--text-tertiary" : undefined)});
   background-color: ${(props) => (props.isActive ? "white" : undefined)};
+
+  @media screen and (min-width: 768px) {
+    width: 60px;
+    height: 60px;
+    font-size: 24px;
+    line-height: 28px;
+  }
 `;
 const TechnologyTextContainer = styled.div`
   display: flex;
@@ -94,6 +139,10 @@ const TechnologyTextContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 327px;
+
+  @media screen and (min-width: 768px) {
+    width: 458px;
+  }
 `;
 const TechnologyStaticHeader = styled.div`
   color: var(--text-secondary);
@@ -104,6 +153,12 @@ const TechnologyStaticHeader = styled.div`
   text-align: center;
   letter-spacing: 2.3625px;
   margin-bottom: 9px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 19px;
+    letter-spacing: 2.7px;
+  }
 `;
 const TechnologyTerminology = styled.div`
   font-family: var(--special-font);
@@ -114,6 +169,11 @@ const TechnologyTerminology = styled.div`
   text-align: center;
   text-transform: uppercase;
   margin-bottom: 16px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 40px;
+    line-height: 46px;
+  }
 `;
 const TechnologyDescription = styled.div`
   font-family: var(--body-font);
@@ -123,6 +183,12 @@ const TechnologyDescription = styled.div`
   font-size: 15px;
   line-height: 25px;
   text-align: center;
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 28px;
+    margin-bottom: 97px;
+  }
 `;
 
 const Technology = () => {
